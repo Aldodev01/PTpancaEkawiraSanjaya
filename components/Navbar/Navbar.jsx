69f01359d7388fb02d4ac5bdb7f24918fafd3325
Aldodev01 <aldodevox@gmail.com>
2021-01-19 21:42:34 +0700

    update Aldo

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Image from 'next/image'
+import "./navbar.module.css"
+
+function Navbar() {
+    return (
+
+        <nav className="navbar">
+            <div className="navLeft">
+            <i class="fas fa-bars menuNav"></i>
+                <div className="imgGroups">
+                <Image
+                    src="/Assets/Brand.png"
+                    alt="Picture of the author"
+                    width="65"
+                    height="65"
+                    />
+                    <div className="textbrand">
+                        <h3 className="textbrand1">PANCA EKAWIRA</h3>
+                        <h3 className="textbrand2">SANJAYA</h3>
+                    </div> 
+                </div>
+                <i class="fas fa-search searchIconPhone"></i>
+
+            </div>
+
+            <div className="navRight">
+                <h3>COMPANY PROFILE</h3>
+                <h3>PRODUCT</h3>
+                <h3>PROJECT</h3>
+                <h3>CONTACT</h3>
+                <h3>E-COMM PLAN</h3>
+                <i class="fas fa-search searchIconNoteBook"></i>
+
+                    <form className="navSearch">
+                        <input type="text" className="navInput" placeholder="search..." />
+                        <div className="icnContainer">
+                        <span className="material-icons">search</span>
+                        </div>
+                        
+                    </form>
+
+            </div>
+        </nav>
+
+    )
+}
+export default Navbar

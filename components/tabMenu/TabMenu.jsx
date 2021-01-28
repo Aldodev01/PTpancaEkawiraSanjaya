2010808b08a6d870135bf5a76d8cb988fa15097b
ricoagustinus123 <rico_a.m@outlook.com>
2021-01-29 00:22:43 +0700

    update tabmenu 29012021

--- components/tabMenu/TabMenu.jsx
+++ components/tabMenu/TabMenu.jsx
@@ -5,10 +5,18 @@ const TabMenu = ({show}) =>{
         <div className="modalNav" style={{
             transform : show ? 'translatey(0)' : 'translateY(-90vh)',
         }}>
-            <h3>COMPANY PROFILE</h3>
-            <h3>PRODUCT</h3>
-            <h3>PROJECT</h3>
-            <h3>CONTACT</h3>
+            <h3 onClick={()=>{
+                window.location.href = "/company-profile"
+            }}>COMPANY PROFILE</h3>
+            <h3 onClick={()=>{
+                window.location.href = "/product"
+            }}>PRODUCT</h3>
+            <h3 onClick={()=>{
+                window.location.href = "/project"
+            }}>PROJECT</h3>
+            <h3 onClick={()=>{
+                window.location.href = "/contact"
+            }}>CONTACT</h3>
             <h3 onClick={()=>{
                 window.location.href = "/ecommplan"
             }}>E-COMM PLAN</h3>

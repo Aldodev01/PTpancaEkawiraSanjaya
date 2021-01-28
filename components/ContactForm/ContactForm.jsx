19525b35bfea2192b4d049ad07f48b802ee61a83
ricoagustinus123 <rico_a.m@outlook.com>
2021-01-29 00:47:15 +0700

    update

--- components/ContactForm/ContactForm.jsx
+++ components/ContactForm/ContactForm.jsx
@@ -38,16 +38,16 @@ const ContactForm = ()=>{
             <form action="" name="google-sheet" className="form-isi" onSubmit={handleForm}>
               
               <label htmlFor="nama">NAMA</label>
-              <input type="text" name="nama" id="nama" className="nama" placeholder="Your Name" />
+              <input required type="text" name="nama" id="nama" className="nama" placeholder="Your Name" />
 
               <label htmlFor="email">EMAIL</label>
-              <input type="email" name="email" id="email" className="email" placeholder="Your Email"/>
+              <input required type="email" name="email" id="email" className="email" placeholder="Your Email"/>
 
               <label htmlFor="subject">SUBJECT</label>
-              <input type="text" name="subject" id="subject" className="subject" placeholder="Your Subject"/>
+              <input required type="text" name="subject" id="subject" className="subject" placeholder="Your Subject"/>
 
               <label htmlFor="pesan">PESAN</label>
-              <textarea name="pesan" id="pesan" cols="30" rows="10" placeholder="Message"></textarea>
+              <textarea required name="pesan" id="pesan" cols="30" rows="10" placeholder="Message"></textarea>
 
               <button className="btn-submit" type="submit" >SUBMIT</button>
             </form>

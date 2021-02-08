5ec9d2dd580595aecc5ff0291e51fa3a46d8d139
name <name@gmail.com>
2021-02-08 23:18:12 +0700

    Rangga Arsy Prawira

--- pages/ecommplan/index.jsx
+++ pages/ecommplan/index.jsx
@@ -16,6 +16,8 @@ const Index = () =>{
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
         rel="stylesheet"></link>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossOrigin="anonymous"></link>
+        <meta name="description" content="PT Panca Ekawira Sanjaya"/>
+        <meta property="og:image" content="/Assets/hero.png" />
 
       </Head>
 

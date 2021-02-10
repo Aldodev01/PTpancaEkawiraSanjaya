1054ada2cd90de47608149e1b2854fcd1020561e
fathurzoy <fathurrahmanf123@gmail.com>
2021-02-10 17:30:29 +0700

    update desc fathur

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-// import Footer from '../../components/footer/Footer'
-// import Navbar from '../../components/Navbar/Navbar'
 import ProductCard from '../../components/Product/productCard/ProductCard'
 import ProductHeader from '../../components/Product/productHeader/ProductHeader'
 import Head from 'next/head'
@@ -15,9 +13,12 @@ function index() {
         <Head>
             <title>PancaEkawiraSanjaya</title>
             <link rel="icon" href="/iconBrand.svg" />
-            <meta name="Product" content=""/>
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet"></link>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossOrigin="anonymous"></link>
+            <meta property="og:image" content="/Assets/hero.png" />
+            <meta name="description" content="PT Panca Ekawira Sanjaya"/>
+
         </Head>
 
            <Layout>

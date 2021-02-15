fde6a12d744074a3647cb88d0e476d83ca1a68d5
Aldodev01 <aldodevox@gmail.com>
2021-02-15 21:17:40 +0700

    aldoUpdate

--- components/businessPartners/BusinessPartners.jsx
+++ components/businessPartners/BusinessPartners.jsx
@@ -9,34 +9,44 @@ function BusinessPartners() {
                 <h1 className="businessPartnersText">OUR BUSINESS PARTNERS</h1>
             <div className="imgContainerBusiness">
                 <div className="imgContainerBottom">
-                    <Image
-                        src="/assets/logoPerusahaan/logoWeber.png"
-                        alt="Picture of the author"
-                        width="400px"
-                        height="400px"
-                    />
-                    <Image
-                        src="/assets/logoPerusahaan/logoMilan.png"
-                        alt="Picture of the author"
-                        width="400px"
-                        height="400px"
+                    <div className="image1">
+                        <Image
+                            src="/assets/logoPerusahaan/logoWeber.png"
+                            alt="Picture of the author"
+                            width="400px"
+                            height="400px"
+                        />
+                    </div>
 
-                    />
-                    <Image
-                        src="/assets/logoPerusahaan/logoShera.png"
-                        alt="Picture of the author"
-                        width="400px"
-                        height="400px"
+                    <div className="image2">
+                        <Image
+                            src="/assets/logoPerusahaan/logoMilan.png"
+                            alt="Picture of the author"
+                            width="400px"
+                            height="400px"
 
-                    />
-                    <Image
-                        src="/assets/logoPerusahaan/logoPaldeco.png"
-                        alt="Picture of the author"
-                        width="400px"
-                        height="400px"
+                        />
+                    </div>
 
-                    />
+                    <div className="image3">
+                        <Image
+                            src="/assets/logoPerusahaan/logoShera.png"
+                            alt="Picture of the author"
+                            width="400px"
+                            height="400px"
 
+                        />
+                    </div>
+                
+                    <div className="image4">
+                        <Image
+                            src="/assets/logoPerusahaan/logoPaldeco.png"
+                            alt="Picture of the author"
+                            width="400px"
+                            height="400px"
+
+                        />
+                    </div>
                 </div>
             </div>
         </div>
